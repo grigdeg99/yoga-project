22bc2efa5c3a97cd7b128d70c97c2ce8bf727cd2
Alex <example@gmail.com>
2021-03-18 17:33:57 +0300

    yoga project

---
+++ js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener("DOMContentLoaded", function () {
+	"use strict";
+
+	let tabsParent = document.querySelector(".info-header"),
+		tabs = document.querySelectorAll(".info-header-tab"),
+		tabContent = document.querySelectorAll(".info-tabcontent");
+
+
+	function hideTabContent(a) {
+
+		for (let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove("show");
+			tabContent[i].classList.add("hide");
+		}
+
+	}
+	hideTabContent(1);
+
+	function showTabContent(b) {
+		if (tabContent[b].classList.contains("hide")) {
+			tabContent[b].classList.remove("hide");
+			tabContent[b].classList.add("show");
+		}
+	}
+
+	tabsParent.addEventListener("click", function (event) {
+		if (event.target && event.target.classList.contains("info-header-tab")) {
+			for (let i = 0; i < tabs.length; i++) {
+				if (event.target == tabs[i]) {
+					hideTabContent(0);
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
+
+
+
+
+
+
+});
